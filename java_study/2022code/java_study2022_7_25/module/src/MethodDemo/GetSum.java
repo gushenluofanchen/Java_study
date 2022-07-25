a2f69f88cb67bb2b53d1772374c0f02da78a2527
王瑞 <3170446830@qq.com>
2022-07-25 12:08:29 +0800

    添加java_study2022_7_25

---
+++ java_study/2022code/java_study2022_7_25/module/src/MethodDemo/GetSum.java
@@ -0,0 +1,16 @@
+package MethodDemo;
+
+public class GetSum {
+    //求和打印
+    public static void main(String[] args) {
+        GetSum();
+    }
+
+    public static void GetSum(){
+        int a=10;
+        int b=20;
+
+        int result=a+b;
+        System.out.println(result);
+    }
+}
---
+++ java_study/2022code/java_study2022_7_25/module/src/MethodDemo/MethodDemo2.java
@@ -0,0 +1,11 @@
+package MethodDemo;
+
+public class MethodDemo2 {
+    public static void main(String[] args) {
+        GetSum(10,20);
+    }
+    public static void GetSum(int a,int b){
+        int result=a+b;
+        System.out.println(result);
+    }
+}
